36f1f7b0c21be8ced801772f0e72ccab18fa39b5
bob <301085344@qq.com>
2018-08-23 22:31:30 +0800

    提交~~

--- server/app/controller/auth.js
+++ server/app/controller/auth.js
@@ -29,13 +29,13 @@ class AuthController extends Base {
 
 		const sessionData = await ctx.curl('https://api.weixin.qq.com/sns/jscode2session',options);
 		if (!sessionData.data.openid) {
-		  return this.fail('登录失败');
+		  return this.fail('登录失败1');
 		}
 		// 验证用户信息完整性（数字签名验证）
 		const crypto = require('crypto');
 		const sha1 = crypto.createHash('sha1').update(fullUserInfo.rawData + sessionData.data.session_key).digest('hex');
 		if (fullUserInfo.signature !== sha1) {
-		  return this.fail('登录失败');
+		  return this.fail('登录失败2');
 		}
 		//根据openid查找用户是否已经注册
 		let userMsg = await ctx.service.user.find({openid: sessionData.data.openid});
